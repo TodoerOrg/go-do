d62250faa0c80d52516ba74f1849ae7f733d731f
pawel-gorka <git@pawelgorka.com>
2017-09-02 21:16:02 +0200

    an http get request test

--- tests/integration/api/hello-world-request_test.go
+++ tests/integration/api/hello-world-request_test.go
@@ -1,9 +1,25 @@
 package apitests
 
 import (
+	"net/http"
 	"testing"
 )
 
+const port = ":8080"
+
+func checkStatus(t *testing.T, r *http.Response) {
+	if r.StatusCode != http.StatusOK {
+		t.Errorf("response was not a 200(OK) ")
+	}
+}
+
 func TestHelloWorldRequest(t *testing.T) {
-	// t.Fail()
+	url := "http://localhost:8080"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		t.Fatalf("Failed to get: %s %v", url, err)
+	}
+
+	checkStatus(t, resp)
 }
